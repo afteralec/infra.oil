5eea51418675d24215b5461f7e9e38612b7e4a9a
Alec DuBois <after.alec@gmail.com>
2023-10-22 11:35:29 -0700

    Add A Name record for play.oilmud.io and request certbot cert

--- dns.tf
+++ dns.tf
@@ -3,9 +3,16 @@ resource "digitalocean_domain" "default" {
    ip_address = digitalocean_droplet.server.ipv4_address
 }
 
-resource "digitalocean_record" "cname" {
+resource "digitalocean_record" "www" {
   domain = digitalocean_domain.default.name
   type = "CNAME"
   name = "www"
   value = "@"
+}
+
+resource "digitalocean_record" "play" {
+  domain = digitalocean_domain.default.id
+  type   = "A"
+  name   = "play"
+  value  = digitalocean_droplet.server.ipv4_address
 }
