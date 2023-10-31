1e063fccd2508fd544330e6475eb7dda41386b9e
Alec DuBois <after.alec@gmail.com>
2023-10-31 11:14:19 -0700

    Add MX and TXT records

--- dns.tf
+++ dns.tf
@@ -1,13 +1,13 @@
 resource "digitalocean_domain" "default" {
-   name = "petrichormud.com"
-   ip_address = digitalocean_droplet.server.ipv4_address
+  name       = "petrichormud.com"
+  ip_address = digitalocean_droplet.server.ipv4_address
 }
 
 resource "digitalocean_record" "www" {
   domain = digitalocean_domain.default.name
-  type = "CNAME"
-  name = "www"
-  value = "@"
+  type   = "CNAME"
+  name   = "www"
+  value  = "@"
 }
 
 resource "digitalocean_record" "play" {
@@ -15,4 +15,30 @@ resource "digitalocean_record" "play" {
   type   = "A"
   name   = "play"
   value  = digitalocean_droplet.server.ipv4_address
+}
+
+resource "digitalocean_record" "mxone" {
+  ttl      = 10800
+  domain   = digitalocean_domain.default.id
+  type     = "MX"
+  name     = "@"
+  priority = 10
+  value    = "spool.mail.gandi.net."
+}
+
+resource "digitalocean_record" "mxtwo" {
+  ttl      = 10800
+  domain   = digitalocean_domain.default.id
+  type     = "MX"
+  name     = "@"
+  priority = 50
+  value    = "fb.mail.gandi.net."
+}
+
+resource "digitalocean_record" "txt" {
+  ttl    = 10800
+  domain = digitalocean_domain.default.id
+  type   = "TXT"
+  name   = "@"
+  value  = "v=spf1 include:_mailcust.gandi.net ?all"
 }
